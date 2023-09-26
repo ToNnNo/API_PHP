b6b4ecbb646e3d6c6bfb78d72b1accb07dfa03b9
Stéphane Menut <smenut@dawan.fr>
2023-09-26 12:00:35 +0200

    add cors headers + options preflight

--- config.php
+++ config.php
@@ -4,6 +4,15 @@ ini_set('display_errors', true);
 error_reporting(E_ALL);
 ini_set('date.timezone', 'Europe/Paris');
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    exit(0);
+}
+
 return [
     'root_directory' => __DIR__
 ];
